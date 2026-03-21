0c3f7c6b364c40589d5f8301dbdfc90e62fb850a
RewerYart <lyalin.vadim06@gmail.com>
2026-03-21 15:55:30 +0200

    .

--- app/oauth.py
+++ app/oauth.py
@@ -9,7 +9,7 @@ import httpx
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "")
 GOOGLE_REDIRECT_URI = os.getenv(
-    "GOOGLE_REDIRECT_URI", "http://localhost:8000/api/auth/google/callback"
+    "GOOGLE_REDIRECT_URI", "http://spotifymvpclone-production.up.railway.app/api/auth/google/callback"
 )
 GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/v2/auth"
 GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"
@@ -19,7 +19,7 @@ GOOGLE_USERINFO_URL = "https://www.googleapis.com/oauth2/v2/userinfo"
 GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID", "")
 GITHUB_CLIENT_SECRET = os.getenv("GITHUB_CLIENT_SECRET", "")
 GITHUB_REDIRECT_URI = os.getenv(
-    "GITHUB_REDIRECT_URI", "http://localhost:8000/api/auth/github/callback"
+    "GITHUB_REDIRECT_URI", "http://spotifymvpclone-production.up.railway.app/api/auth/github/callback"
 )
 GITHUB_AUTH_URL = "https://github.com/login/oauth/authorize"
 GITHUB_TOKEN_URL = "https://github.com/login/oauth/access_token"
